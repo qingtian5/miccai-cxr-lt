bf1166d00ad1bb0eb09ef73f5cdab0a4cfd2b3dc
root <root@cv-panxuhao-s1hghj-0.cv-panxuhao-s1hghj.lizr-a.svc.dev00-bcebj.local>
2024-06-24 09:17:48 +0800

    add prediction

--- working/prediction/scripts.sh
+++ working/prediction/scripts.sh
@@ -2,3 +2,4 @@
 
 python model.py --checkpoint=epoch_3.pth --config=swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e3.py
 
+python model.py --checkpoint=epoch_15.pth --config=mae_vit-large-mae-e1600-ft-unbalanced-data_e15.py
