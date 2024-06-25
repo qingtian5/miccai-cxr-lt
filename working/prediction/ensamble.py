178ba73ff64aea31ab5253715857f945bff91948
root <root@cv-panxuhao-s1hghj-0.cv-panxuhao-s1hghj.lizr-a.svc.dev00-bcebj.local>
2024-06-25 09:43:03 +0800

    adj ensamble

--- working/prediction/ensamble.py
+++ working/prediction/ensamble.py
@@ -11,28 +11,44 @@ def get_results(result_file_path):
     return res
 
 if __name__ == "__main__":
+    # folders = [
+    #     "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/mae_vit-large-mae-ft-unbalanced-data/epoch_36",
+    #     "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data/epoch_12",
+    #     "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e6/epoch_6",
+    #     "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e12/epoch_12",
+    #     "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e12_copy1/epoch_12",
+    # ]
+
     folders = [
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/mae_vit-large-mae-ft-asymmetricloss-unbalanced-data/epoch_33",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/mae_vit-large-mae-ft-unbalanced-data/epoch_36",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/mae_vit-large-mae-ft-unbalanced-data/epoch_30",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/mae_vit-large-mae-ft-unbalanced-data/epoch_27",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swin-large-ft-focalloss-unbalanced-data/epoch_12",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swin-large-ft-focalloss-unbalanced-data/epoch_6",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swin-large-ft-focalloss-unbalanced-data/epoch_9",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-base-ft-focalloss-unbalanced-data/epoch_36",
         "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data/epoch_12",
-        # "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/mae_vit-large-mae-ft-unbalanced-data/epoch_36",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data/epoch_36",
         "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data/epoch_12",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data/epoch_6",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e3/epoch_3",
         "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e6/epoch_6",
-        # "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e3/epoch_3",
-        # "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e6/epoch_5",
         "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e12/epoch_12",
-        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e12/epoch_9",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e12/epoch_10",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e12/epoch_11",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e12_copy1/epoch_11",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e12_copy1/epoch_12",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/swinv2-large-ft-focalloss-unbalanced-data_alltrain_data_e15/epoch_15",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/mae_vit-large-mae-e1600-ft-unbalanced-data_e15/epoch_15",
+        "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission/mae_vit-large-mae-e1600-ft-unbalanced-data_e12/epoch_12",
     ]
+
     results_file_name = "results.txt"
     csv_name = "development_task1_sample_submission.csv"
     all_dfs = []
     all_res = []
-    for f in folders:
-        result_file_path = os.path.join(f, results_file_name)
-        res = get_results(result_file_path)
-        df = pd.read_csv(os.path.join(f, csv_name))
-        all_dfs.append(df)
-        all_res.append(res)
-        print(f, res)
-    
+
     des_folder = "/mnt/pfs-mc0p4k/cv/team/panxuhao/playground/miccai24_cxr_lt/code/prediction/submission_ensamble"
     now = datetime.now()
     timestamp = now.strftime('%Y-%m-%d_%H-%M-%S')
@@ -40,8 +56,15 @@ if __name__ == "__main__":
     os.makedirs(output_folder, exist_ok=True)
 
     with open(os.path.join(output_folder, "raw_results.txt"), "w") as file:
-        for f, r in zip(folders, all_res):
-            file.write(f"{f}: {r}\n")
+        for f in folders:
+            result_file_path = os.path.join(f, results_file_name)
+            res = get_results(result_file_path)
+            if res > 0.23:
+                file.write(f"{f}: {res}\n")
+                df = pd.read_csv(os.path.join(f, csv_name))
+                all_dfs.append(df)
+                all_res.append(res)
+                print(f, res)
             
     total_res = sum(all_res)
     all_res = [res / total_res for res in all_res]
